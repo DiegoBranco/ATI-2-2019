a5104c2ea9ac39b8aab373535be39c73b7f87564
adrianjmejias <adrianjmejias@gmail.com>
2020-01-26 23:39:39 -0400

    added error msgs

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_debug import Debug
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+Debug(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 from app import routes
