ad3262b335bf5ecf65d75a2ab476a82a1719adde
adrianjmejias <adrianjmejias@gmail.com>
2020-02-26 11:19:27 -0400

    deliver

--- Frontend/app/forms/__init__.py
+++ Frontend/app/forms/__init__.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.fields import TextAreaField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -7,3 +8,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class EditorForm(FlaskForm):
+
+    description = TextAreaField("Description", validators=[DataRequired()])
+
+    submit = SubmitField('Save Changes')
+    
